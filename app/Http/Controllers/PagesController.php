1126c614af78cd20158355a6a184a91ea040b883
zainalwan <zainalwan4@gmail.com>
2020-12-06 21:44:33 +0700

    Add license header text to PagesController

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -1,5 +1,11 @@
 <?php
 
+/* 
+ * Code that written below is belong to Zain Alwan Wima Irfani. You may
+ * not use, share, modify, and study without the author's permission
+ * (zainalwan4@gmail.com).
+ *  */
+
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
