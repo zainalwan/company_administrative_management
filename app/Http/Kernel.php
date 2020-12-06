cdd0e8d60b4d87d35e34842c5644560c3e22b932
zainalwan <zainalwan4@gmail.com>
2020-12-06 21:20:06 +0700

    Registering is_not_logged_in middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,5 +63,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
 		'is_logged_in' => Middleware\BeforeMiddlewareIsLoggedIn::class,
+		'is_not_logged_in' => Middleware\BeforeMiddlewareIsNotLoggedIn::class,
     ];
 }
