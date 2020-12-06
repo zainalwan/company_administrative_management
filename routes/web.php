b74ad050e5ce350c98d400c4a4754d9e1ebd6065
zainalwan <zainalwan4@gmail.com>
2020-12-06 20:33:12 +0700

    Create log out route

--- routes/web.php
+++ routes/web.php
@@ -27,4 +27,6 @@ Route::post('register', [AdminController::class, 'store']);
 Route::get('login', [AdminController::class, 'login']);
 Route::post('login', [AdminController::class, 'authenticate']);
 
+Route::get('log_out', [AdminController::class, 'log_out']);
+
 Route::get('/', [PagesController::class, 'index']);
