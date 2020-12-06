887afd3c4f26294c06d287d97e580b729d61208a
zainalwan <zainalwan4@gmail.com>
2020-12-06 20:57:34 +0700

    Add use statement for PagesController inside route file

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\PagesController;
 
 /*
    |--------------------------------------------------------------------------
