0d0def3857740e953447a591cec728f62bd19f24
zainalwan <zainalwan4@gmail.com>
2020-12-06 20:20:48 +0700

    Add routes to login and register page

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,28 @@
 <?php
 
+/* 
+ * Code that written below is belong to Zain Alwan Wima Irfani. You may
+ * not use, share, modify, and study without the author's permission
+ * (zainalwan4@gmail.com).
+ *  */
+
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\AdminController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/*
+   |--------------------------------------------------------------------------
+   | Web Routes
+   |--------------------------------------------------------------------------
+   |
+   | Here is where you can register web routes for your application. These
+   | routes are loaded by the RouteServiceProvider within a group which
+   | contains the "web" middleware group. Now create something great!
+   |
+ */
+
+Route::get('register', [AdminController::class, 'register']);
+Route::post('register', [AdminController::class, 'store']);
+
+Route::get('login', [AdminController::class, 'login']);
+Route::post('login', [AdminController::class, 'authenticate']);
