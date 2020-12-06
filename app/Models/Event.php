99a58108972625ea4c3be52e6e349b18d0c08c8f
zainalwan <zainalwan4@gmail.com>
2020-12-06 21:46:22 +0700

    Create event controller and model

---
+++ app/Models/Event.php
@@ -0,0 +1,17 @@
+<?php
+
+/* 
+ * Code that written below is belong to Zain Alwan Wima Irfani. You may
+ * not use, share, modify, and study without the author's permission
+ * (zainalwan4@gmail.com).
+ *  */
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Event extends Model
+{
+    use HasFactory;
+}
