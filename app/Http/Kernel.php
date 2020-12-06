9490087838d89b5d0f9db92b255f8eef18224fb7
zainalwan <zainalwan4@gmail.com>
2020-12-06 21:08:50 +0700

    Correcting middleware namespace

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -62,6 +62,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-		'is_logged_in' => App\Http\Middleware\BeforeMiddlewareIsLoggedIn::class,
+		'is_logged_in' => Middleware\BeforeMiddlewareIsLoggedIn::class,
     ];
 }
