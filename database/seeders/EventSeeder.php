576a963a0828da0652a02345710d992ad1ff2e9b
zainalwan <zainalwan4@gmail.com>
2020-12-07 11:40:38 +0700

    Create EventSeeder

---
+++ database/seeders/EventSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+/* 
+ * Code that written below is belong to Zain Alwan Wima Irfani. You may
+ * not use, share, modify, and study without the author's permission
+ * (zainalwan4@gmail.com).
+ *  */
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class EventSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
