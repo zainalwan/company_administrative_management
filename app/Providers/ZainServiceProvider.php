44f6d1bd7d4df4f5b364c3094563837f73522c65
zainalwan <zainalwan4@gmail.com>
2020-12-07 10:21:17 +0700

    Create license header text and bind Random library class

--- app/Providers/ZainServiceProvider.php
+++ app/Providers/ZainServiceProvider.php
@@ -1,8 +1,16 @@
 <?php
 
+/* 
+ * Code that written below is belong to Zain Alwan Wima Irfani. You may
+ * not use, share, modify, and study without the author's permission
+ * (zainalwan4@gmail.com).
+ *  */
+
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+
+use \App\Library\Zain\Random;
 
 class ZainServiceProvider extends ServiceProvider
 {
@@ -13,7 +21,9 @@ class ZainServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(Random::class, function($app) {
+			return new Random;
+		});
     }
 
     /**
