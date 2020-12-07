755bf8273ac1dc08791258d5b6028855cd22963b
zainalwan <zainalwan4@gmail.com>
2020-12-07 16:53:49 +0700

    Create announcement seeder and migration

---
+++ database/migrations/2020_12_07_164613_create_announcements_table.php
@@ -0,0 +1,39 @@
+<?php
+
+/* 
+ * Code that written below is belong to Zain Alwan Wima Irfani. You may
+ * not use, share, modify, and study without the author's permission
+ * (zainalwan4@gmail.com).
+ *  */
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAnnouncementsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('announcements', function (Blueprint $table) {
+            $table->id();
+			$table->string('name', 255);
+			$table->string('description', 10000);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('announcements');
+    }
+}
---
+++ database/seeders/AnnouncementSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+/* 
+ * Code that written below is belong to Zain Alwan Wima Irfani. You may
+ * not use, share, modify, and study without the author's permission
+ * (zainalwan4@gmail.com).
+ *  */
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use \App\Library\Zain\Random;
+
+class AnnouncementSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Random $random)
+    {
+        for($i = 0; $i < 30; $i++)
+		{
+			DB::table('events')->insert([
+				'name' => $random->make()->name(),
+				'description' => $random->make()->paragraph(2),
+				'created_at' => date('Y-m-d H:i'),
+				'updated_at' => date('Y-m-d H:i')
+			]);
+		}
+    }
+}
