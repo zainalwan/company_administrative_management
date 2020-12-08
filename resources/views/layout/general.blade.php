76ee92c86bb51f2b493fa077ab6230ddd1e9f110
zainalwan <zainalwan4@gmail.com>
2020-12-09 06:43:46 +0700

    Modify link in navbar

--- resources/views/layout/general.blade.php
+++ resources/views/layout/general.blade.php
@@ -46,10 +46,10 @@
 					<a href="/employee">Employee</a>
 				</li>
 				<li>
-					<a href="/admin"><svg width="1em" height="1em" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+					<a href="/account"><svg width="1em" height="1em" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
 						<path fill-rule="evenodd" d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
 					</svg></a>
-					<a href="/admin">Account</a>
+					<a href="/account">Account</a>
 				</li>
 				<li>
 					<a href="/log_out"><svg width="1em" height="1em" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
