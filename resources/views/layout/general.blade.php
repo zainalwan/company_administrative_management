d57bc04068eff31ccb635aeb569b650ea2af4cab
zainalwan <zainalwan4@gmail.com>
2020-12-08 22:25:35 +0700

    Modify general layout

--- resources/views/layout/general.blade.php
+++ resources/views/layout/general.blade.php
@@ -8,7 +8,7 @@
 
 <html>
 	<head>
-		<title>LIPSUM</title>
+		<title>{{ $title }}</title>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta name="author" content="Zain Alwan Wima Irfani">
