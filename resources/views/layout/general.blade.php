2e0c2b03fab0d180f7be2fa051a0a95779eb2b90
zainalwan <zainalwan4@gmail.com>
2020-12-11 11:08:01 +0700

    Modify css link

--- resources/views/layout/general.blade.php
+++ resources/views/layout/general.blade.php
@@ -13,7 +13,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta name="author" content="Zain Alwan Wima Irfani">
 		<meta name="description" content="Company administrative management web app.">
-        <link rel="stylesheet" href="assets/css/app.css">
+        <link rel="stylesheet" href="/assets/css/app.css">
 	</head>
 
 	<body>
