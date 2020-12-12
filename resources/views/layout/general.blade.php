61d5e3c1c64215feb3442b77a4a5021a72534688
zainalwan <zainalwan4@gmail.com>
2020-12-12 13:18:42 +0700

    Modify general template view

--- resources/views/layout/general.blade.php
+++ resources/views/layout/general.blade.php
@@ -25,32 +25,32 @@
 						<path d="M6.5 10.995V14.5a.5.5 0 0 1-.5.5H2a.5.5 0 0 1-.5-.5v-7a.5.5 0 0 1 .146-.354l6-6a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 .146.354v7a.5.5 0 0 1-.5.5h-4a.5.5 0 0 1-.5-.5V11c0-.25-.25-.5-.5-.5H7c-.25 0-.5.25-.5.495z"/>
 						<path fill-rule="evenodd" d="M13 2.5V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"/>
 					</svg></a>
-					<a href="/">Home</a>
+					<a class="" href="/">Home</a>
 				</li>
 				<li>
-					<a href="/event"><svg width="1em" height="1em" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+					<a class="{{ preg_match('/event/', url()->current()) ? 'active' : false }}" href="/event"><svg width="1em" height="1em" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
 						<path fill-rule="evenodd" d="M14 0H2a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zM1 3.857C1 3.384 1.448 3 2 3h12c.552 0 1 .384 1 .857v10.286c0 .473-.448.857-1 .857H2c-.552 0-1-.384-1-.857V3.857z"/>
 						<path fill-rule="evenodd" d="M12 7a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/>
 					</svg></a>
-					<a href="/event">Event</a>
+					<a class="{{ preg_match('/event/', url()->current()) ? 'active' : false }}" href="/event">Event</a>
 				</li>
 				<li>
-					<a href="/announcements"><svg width="1em" height="1em" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+					<a class="{{ preg_match('/announcement/', url()->current()) ? 'active' : false }}" href="/announcement"><svg width="1em" height="1em" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
 						<path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zm.995-14.901a1 1 0 1 0-1.99 0A5.002 5.002 0 0 0 3 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901z"/>
 					</svg></a>
-					<a href="/announcement">Announcement</a>
+					<a class="{{ preg_match('/announcement/', url()->current()) ? 'active' : false }}" href="/announcement">Announcement</a>
 				</li>
 				<li>
-					<a href="/employee"><svg width="1em" height="1em" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+					<a class="{{ preg_match('/employee/', url()->current()) ? 'active' : false }}" href="/employee"><svg width="1em" height="1em" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
 						<path fill-rule="evenodd" d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H7zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm-5.784 6A2.238 2.238 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.325 6.325 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1h4.216zM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5z"/>
 					</svg></a>
-					<a href="/employee">Employee</a>
+					<a class="{{ preg_match('/employee/', url()->current()) ? 'active' : false }}" href="/employee">Employee</a>
 				</li>
 				<li>
-					<a href="/account"><svg width="1em" height="1em" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+					<a class="{{ preg_match('/account/', url()->current()) ? 'active' : false }}" href="/account"><svg width="1em" height="1em" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
 						<path fill-rule="evenodd" d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
 					</svg></a>
-					<a href="/account">Account</a>
+					<a class="{{ preg_match('/account/', url()->current()) ? 'active' : false }}" href="/account">Account</a>
 				</li>
 				<li>
 					<a href="/log_out"><svg width="1em" height="1em" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
